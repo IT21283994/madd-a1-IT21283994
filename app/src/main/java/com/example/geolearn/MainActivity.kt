76011dd2d5529889fbb4c29b846b19fa32503798
IT21283994 <it21283994@my.sliit.lk>
2024-10-05 12:20:54 +0530

    firebase initialized

---
+++ app/src/main/java/com/example/geolearn/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.geolearn
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val secondActbutton = findViewById<Button>(R.id.button)
+        secondActbutton.setOnClickListener {
+            val Intent = Intent(this,SignUp::class.java)
+            startActivity(Intent)
+        }
+        }
+    }
